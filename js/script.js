697fe7c2301e5b05fe909c3e20b9e7713194a9c0
Bart Soete <bart.soete@howest.be>
2019-10-05 09:29:51 +0200

    Add load functionality

--- js/script.js
+++ js/script.js
@@ -2,22 +2,32 @@
 window.addEventListener('load', Initieer);
 
 // Globale Variabelen
-
+let txtId,txtNaam,divFeedback;
 
 // StartUP
 function Initieer() {
   // Lokale variabelen
-
+  let btnVerbergElementMetID;
   // Inlezen DOM-elementen
-
-
+  btnVerbergElementMetID = document.querySelector('#btnVerbergElementMetId');
+  divFeedback = document.querySelector('#divFeedback');
+  txtNaam = document.querySelector('#txtNaam');
+  txtId = document.querySelector('#txtId');
   // Toevoegen Event-Listeners
+  btnVerbergElementMetID.addEventListener('click',ToonVerbergElementViaId);
 
-
+  // Toevoegen van de laad-invulvelden
+  txtNaam.value = 'Bart';
+  txtId.value = 'txtNaam';
+  divFeedback.style.color = 'red';
 }
 
 function ToonVerbergElementViaId() {
+  let Naam, gekozenId;
+  Naam = txtNaam.value;
+  gekozenId = txtId.value;
 
+  divFeedback.innerHTML += `${Naam} heeft id ${gekozenId} gekozen`;
 }
 
 function ToonVerbergElementViaSelector() {
