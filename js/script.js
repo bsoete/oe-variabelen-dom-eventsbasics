9e60f203d6df9e56df33d6c4fde434a581a287e2
Bart Soete <bart.soete@howest.be>
2019-10-05 10:00:45 +0200

    Added cssSelector Functionality

--- js/script.js
+++ js/script.js
@@ -2,25 +2,28 @@
 window.addEventListener('load', Initieer);
 
 // Globale Variabelen
-let txtId,txtNaam,divFeedback;
+let txtId,txtNaam,txtCssSelector,divFeedback;
 
 // StartUP
 function Initieer() {
   // Lokale variabelen
-  let btnVerbergElementMetID;
+  let btnVerbergElementMetID, btnVerbergElementMetSelector;
   // Inlezen DOM-elementen
   btnVerbergElementMetID = document.querySelector('#btnVerbergElementMetId');
+  btnVerbergElementMetSelector = document.querySelector('#btnVerbergElementMetSelector');
   divFeedback = document.querySelector('#divFeedback');
   txtNaam = document.querySelector('#txtNaam');
   txtId = document.querySelector('#txtId');
+  txtCssSelector = document.querySelector('#txtCssSelector');
   // Toevoegen Event-Listeners
   btnVerbergElementMetID.addEventListener('click',ToonVerbergElementViaId);
-
+  btnVerbergElementMetSelector.addEventListener('click',ToonVerbergElementViaSelector);
   // Toevoegen van de laad-invulvelden
   txtNaam.value = 'Bart';
   txtId.value = 'txtNaam';
   divFeedback.style.color = 'red';
 }
+
 
 function ToonVerbergElementViaId() {
   let Naam, gekozenId, gekozenElement;
@@ -43,7 +46,22 @@ function ToonVerbergElementViaId() {
 }
 
 function ToonVerbergElementViaSelector() {
+  let Naam, gekozenSelector, gekozenElement;
+  Naam = txtNaam.value;
+  gekozenSelector = txtCssSelector.value;
+  gekozenElement = document.querySelector(gekozenSelector);
 
+  if(gekozenElement.style.display == ''){
+    gekozenElement.style.display = 'none';
+  } 
+  else if(gekozenElement.style.display == 'none'){
+    gekozenElement.style.display = 'inline';
+  }
+  else if(gekozenElement.style.display == 'inline'){
+    gekozenElement.style.display = 'none';
+  }
+  
+  divFeedback.innerHTML += `${Naam} heeft id ${gekozenSelector} gewijzigd <br/>`;
 }
 
 
